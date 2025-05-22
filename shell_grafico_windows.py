67c30cf77430b8a4b5859eca1f078f902086e8a6
Danny Fernando Rodríguez <dannyfernando9964@gmail.com>
2025-05-21 21:00:53 -0500

    Primer commit: subiendo archivo para el profe

---
+++ shell_grafico_windows.py
@@ -0,0 +1,21 @@
+﻿import tkinter as tk
+import os
+
+def abrir_navegador():
+    os.system("start chrome")
+
+def abrir_editor():
+    os.system("notepad")
+
+def abrir_terminal():
+    os.system("start powershell")
+
+ventana = tk.Tk()
+ventana.title("Shell Gráfico - Windows")
+ventana.geometry("300x200")
+
+tk.Button(ventana, text="1 Navegador", command=abrir_navegador).pack(pady=10)
+tk.Button(ventana, text="2 Editor de Texto", command=abrir_editor).pack(pady=10)
+tk.Button(ventana, text="3 Terminal", command=abrir_terminal).pack(pady=10)
+
+ventana.mainloop()
